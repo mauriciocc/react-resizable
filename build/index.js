8c4a7c8eaca513060a200fb876f3b78e5ed7e2ff
Ciço <mauriciocc@outlook.com>
2019-04-03 09:56:57 -0300

    added build folder to repo

---
+++ build/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _Resizable = require('./Resizable');
+
+var _Resizable2 = _interopRequireDefault(_Resizable);
+
+var _ResizableBox = require('./ResizableBox');
+
+var _ResizableBox2 = _interopRequireDefault(_ResizableBox);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+    Resizable: _Resizable2.default,
+    ResizableBox: _ResizableBox2.default
+};
